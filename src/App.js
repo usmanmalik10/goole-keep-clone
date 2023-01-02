dc0f563a27dc41c51c244c9cb2d5bef0452c9b16
Muhammad Usman <86666190+usmanmalik10@users.noreply.github.com>
2023-01-02 16:50:46 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import CreateNote from "./CreateNote";
-
+import Note from "./Note";
 const App = () => {
+  const [addItem, setAddItem] = useState([]);
+
+  const addNote = (note) => {
+    // alert("I am clicked");
+   setAddItem((prevData) => {
+    return [...prevData, note];
+   });
+   console.log(note);
+  };
+
+  const onDelete = (id) => {
+    setAddItem((olddata) =>
+    olddata.filter((currdata, indx) => {
+      return indx !== id;
+    })
+    );
+  };
+
+
   return (
     <>
-      <Header />
+      <Header /> 
+      <CreateNote passNote={addNote} />
+    
+      {
+        addItem.map((val, index) => {
+          return (
+             <Note
+          key={index}
+          id={index}
+          title={val.title}
+          content={val.content}
+          deleteItem={onDelete}
+          />
+          );
+        })
+      }
       <Footer />
-      <CreateNote/>
     </>
   );
 };
